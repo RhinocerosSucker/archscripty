7d08d0a6f770730eebdbc46fa9320d4b4567ca05
RhinocerosSucker <129686372+RhinocerosSucker@users.noreply.github.com>
2024-06-15 14:41:55 +0200

    Update script-test.sh

--- script-test.sh
+++ script-test.sh
@@ -2,11 +2,12 @@ sudo pacman -Syu
 sudo pacman -S git
 pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 cd
-sudo pacman -S waybar hyprpaper discord nautilus alacritty 
-sudo pacman -R kitty 
-yay thorium-browser
+sudo pacman -S waybar hyprpaper discord nautilus alacritty pamixer
+sudo pacman -R kitty firefox
+yay thorium-browser pavu otf-font-awesome ttf-arimo-nerd
 git clone https://github.com/RhinocerosSucker/archscripty.git
 cd archscripty
 mv gyattie.tar.xz ~/.config/
 cd ~/.config/
 tar -xf --overwrite gyattie.tar.xz
+mv -f .bashrc ~/
