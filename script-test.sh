7d912a395b19890f749068b8e1bf6a624027b462
RhinocerosSucker <129686372+RhinocerosSucker@users.noreply.github.com>
2024-06-19 20:21:59 +0200

    Update script-test.sh

--- script-test.sh
+++ script-test.sh
@@ -1,13 +1,18 @@
 sudo pacman -Syu
-sudo pacman -S git
 pacman -S --needed git base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
 cd
-sudo pacman -S waybar hyprpaper discord nautilus alacritty pamixer
+sudo pacman -S waybar hyprpaper discord nautilus alacritty pamixer blueman 
+sudo systemctl enable bluetooth
 sudo pacman -R kitty firefox
-yay thorium-browser pavu otf-font-awesome ttf-arimo-nerd
+yay thorium-browser
+yay pavu
+yay otf-font-awesome
+yay ttf-arimo-nerd
+yay jetbrains-mono
 git clone https://github.com/RhinocerosSucker/archscripty.git
 cd archscripty
 mv gyattie.tar.xz ~/.config/
 cd ~/.config/
-tar -xf --overwrite gyattie.tar.xz
+sudo tar -xf --overwrite gyattie.tar.xz
 mv -f .bashrc ~/
+
